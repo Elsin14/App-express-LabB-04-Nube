888d128617d1d77075c8d4379ce56f0742728a97
Elsin14 <elsin.drafus14@gmail.com>
2021-04-01 10:58:24 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,40 @@
+const clientes = [
+    {
+        id: 1,
+        nombre: 'Elsin Vila',
+        email: 'elsin.vila@tecsup.edu.pe',
+    },
+    {
+        id: 2,
+        nombre: 'Yeridi Huaman',
+        email: 'teridi.huaman@tecsup.edu.pe',
+    },
+    {
+        id: 3,
+        nombre: 'Gustavo Blanco',
+        email: 'gustavo.blanco@tecsup.edu.pe',
+    },
+];
+const productos = [
+    {
+        id: 1,
+        nombre: 'mouse gamer',
+        stock: 10,
+        precio: 100,
+    },
+    {
+        id: 2,
+        nombre: 'pantalla LCD',
+        stock: 100,
+        precio: 300,
+    },
+    {
+        id: 3,
+        nombre: 'Silla gamer',
+        stock: 500,
+        precio: 500,
+    },
+];
+
+exports.clientes = clientes;
+exports.productos = productos;
